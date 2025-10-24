44e17271a22c6af978744b83b0a216aaf6528805
marcelo02 <mcfilho2002@gmail.com>
2025-10-24 13:45:53 -0400

    adicionada animacao de ataque

--- extensao/scenes/Scripts/player.gd
+++ extensao/scenes/Scripts/player.gd
@@ -7,13 +7,12 @@ const dash_duration = 0.2
 const gravity = 1200.0
 
 
-
-
 @onready var animation: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var dash_timer: Timer = $DashTimer
 @onready var dash_cooldown_timer: Timer = $DashCooldownTimer
 
+var is_attacking: bool = false
 var is_dashing: bool = false
 var can_dash: bool = true
 var dash_vector: Vector2 = Vector2.ZERO
@@ -40,6 +39,9 @@ func handle_input() -> void:
 		velocity = dash_vector * dash_speed
 		return
 		
+	if Input.is_action_just_pressed("attack") and not is_attacking:
+		start_attack()	
+		
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
 		
@@ -54,6 +56,11 @@ func animate() -> void:
 		if animation.current_animation != "dash":
 			animation.play("dash")
 		return
+		
+	if is_attacking:
+		if animation.current_animation != "attack":
+			animation.play("attack")
+		return	
 		
 	if not is_on_floor():
 		if velocity.y < 0:
@@ -100,12 +107,11 @@ func _on_DashTimer_timeout() -> void:
 func _on_DashCooldownTimer_timeout() -> void:
 	can_dash = true
 	
-		
-	
-	
-	
-		
-		
+func start_attack() -> void: ########
+	is_attacking = true
 	
 
-	
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "attack":
+		is_attacking= false
