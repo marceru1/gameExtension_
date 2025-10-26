d30303d65e6d817533c96c9d1df1fa2708f6ae2b
marcelo02 <mcfilho2002@gmail.com>
2025-10-26 12:18:48 -0400

    refinando movimentos e adicionando area de ataque

--- extensao/scenes/Scripts/player.gd
+++ extensao/scenes/Scripts/player.gd
@@ -1,40 +1,44 @@
 extends CharacterBody2D
 
+
+## DEFININDO OS VALORES DE MOVIMENTACAO
 const speed: float = 300.0
 const jump_velocity = -500.0
 const dash_speed = 900.0
 const dash_duration = 0.2
 const gravity = 1200.0
 
+## VALORES PRA FLUIDEZ DE MOVIMENTACAO
+@export var acceleration: float = 10.0
+@export var desaceleration: float = 12.0
 
+## REFERENCIANDO OS NOS DO LADO
 @onready var animation: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var dash_timer: Timer = $DashTimer
 @onready var dash_cooldown_timer: Timer = $DashCooldownTimer
 
+
+## CONDICOES
 var is_attacking: bool = false
 var is_dashing: bool = false
 var can_dash: bool = true
 var dash_vector: Vector2 = Vector2.ZERO
 
-#func _process(delta: float) -> void:
-	#if is_on_floor():
-		#can_dash = true
 		
+## FUNCAO DA GRAVIDADE, SE NAO TA DASHANDO E NAO TA NO CHAO, TA "CAINDO"
 func _physics_process(delta: float) -> void:
 	if not is_on_floor() and not is_dashing:
 		velocity.y += gravity * delta
-	#if not is_on_floor() and is_dashing:
-		#velocity.y += gravity * delta
-	#elif is_on_floor() and not is_dashing:
-		#velocity.y = 0
+
 		
-	handle_input()
+	handle_input(delta)
 	move_and_slide()
 	flip()
 	animate()
 	
-func handle_input() -> void:
+## O NOME JA DIZ HANDLE INPUT, EH PRA LIDAR COM AS ENTRADAS DO JOGADOR
+func handle_input(delta: float) -> void:
 	if is_dashing:
 		velocity = dash_vector * dash_speed
 		return
@@ -46,11 +50,20 @@ func handle_input() -> void:
 		velocity.y = jump_velocity
 		
 	var input_direction := Input.get_axis("ui_left", "ui_right")
-	velocity.x = input_direction * speed
+	#velocity.x = input_direction * speed
 	
+	##logica de aceleracao e desaceleracao pra mais fluidez dosmovimentos
+	var target_speed = input_direction * speed
+	
+	if input_direction != 0:
+		velocity.x = lerp(velocity.x, target_speed, acceleration * delta)
+	else:
+		velocity.x = lerp(velocity.x , 0.0, desaceleration * delta)
+	######
 	if Input.is_action_just_pressed("dash") and can_dash and not is_dashing:
 		start_dash()
-		
+
+## FUNCOES DE ANIMACAO	
 func animate() -> void:
 	if is_dashing:
 		if animation.current_animation != "dash":
@@ -84,6 +97,7 @@ func flip() -> void:
 	elif velocity.x < 0:
 		sprite.flip_h = true
 		
+## FUNCAO DE DASH
 func start_dash() -> void:
 	is_dashing = true
 	can_dash = false
@@ -103,12 +117,20 @@ func start_dash() -> void:
 	
 func _on_DashTimer_timeout() -> void:
 	is_dashing = false
+	velocity.x = 0.0 ## AQUI DEFINE QUE DEPOIS DO DASH A VELOCIDADE VAI PRA 0
+	## O QUE TIRA AQUELE BUG DE CONTINUAR ANDANDO (ANIMACAO)DEPOIS DO DASH
 	
 func _on_DashCooldownTimer_timeout() -> void:
 	can_dash = true
 	
 func start_attack() -> void: ########
 	is_attacking = true
+	## COMECANDO A LOGICA DE DETECCAO DA HITBOX DE ATAQUE
+	var overlapping_objects = $attack_area.get_overlapping_areas()
+	
+	for area in overlapping_objects:
+		var parent = area.get_parent()
+		print(parent.name)
 	
 
 
